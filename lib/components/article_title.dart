e0a3a6aaf45dcda749c317f3215bb02c9604975c
bigzhu <vermiliondun@gmail.com>
2019-08-22 14:17:22 +0800

    拆分 article title

---
+++ lib/components/article_title.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// 文章对应的 youtube 图标或者头像
+class ArticleYoutubeAvatar extends StatelessWidget {
+  const ArticleYoutubeAvatar({this.youtubeURL, this.avatar, Key key})
+      : super(key: key);
+  final String youtubeURL;
+  final String avatar;
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+        visible: youtubeURL == '' ? false : true,
+        child: avatar == ''
+            ? Icon(
+                FontAwesomeIcons.youtube,
+                color: Colors.red,
+              )
+            : CircleAvatar(
+                backgroundImage: NetworkImage(avatar),
+              ));
+  }
+}
