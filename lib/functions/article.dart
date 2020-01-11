9c4b57f6ea9c645c0c05ac2df8be32131f614d09
bigzhu <vermiliondun@gmail.com>
2020-01-11 15:34:41 +0800

    fix

--- lib/functions/article.dart
+++ lib/functions/article.dart
@@ -22,9 +22,10 @@ bool hasLetter(String str) {
   RegExp regHasLetter = new RegExp(r"[a-zA-Z]+");
   return regHasLetter.hasMatch(str);
 }
+
 bool consecutiveLetter(String str) {
   RegExp regHasLetter = new RegExp(r"[a-zA-Z]{2}");
-  return !regHasLetter.hasMatch(str);
+  return regHasLetter.hasMatch(str);
 }
 
 bool isNeedLearn(Word word) {
@@ -35,6 +36,6 @@ bool isNeedLearn(Word word) {
   // 只有一个字符
   //if (word.text.length == 1) return false;
   // 没有连续的字母
-  if(!consecutiveLetter(word.text)) return false;
+  if (!consecutiveLetter(word.text)) return false;
   return true;
 }
